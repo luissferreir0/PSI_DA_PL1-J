705303e571a184500bb911dd5992a8c2018cc014
Dr0Gh0st <bm39502@gmail.com>
2022-05-16 15:15:39 +0100

    Forms criados

--- RestGest/Form_Menu.cs
+++ RestGest/Form_Menu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RestGest
 {
-    public partial class Form1 : Form
+    public partial class Form_Menu : Form
     {
-        public Form1()
+        public Form_Menu()
         {
             InitializeComponent();
         }
--- RestGest/Program.cs
+++ RestGest/Program.cs
@@ -16,7 +16,7 @@ namespace RestGest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form_Principal());
         }
     }
 }
