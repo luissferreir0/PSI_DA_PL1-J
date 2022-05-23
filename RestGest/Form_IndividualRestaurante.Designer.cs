8346f105fe0bd564e23c921d88c4279e34a3c3e7
Diogo Gonçalves <2211879@my.ipleiria.pt>
2022-05-23 15:45:07 +0100

    Atualização do Form_Principal
    Criação de um novo botão na barra de ferramentas par ao formulário individual de cada restaurante

--- RestGest/Form_IndividualRestaurante.Designer.cs
+++ RestGest/Form_IndividualRestaurante.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
@@ -44,7 +45,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBoxNomeClienteNovo = new System.Windows.Forms.TextBox();
             this.buttonRemoverRestaurante = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox4.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -58,9 +58,11 @@
             this.groupBox1.Controls.Add(this.groupBox4);
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Controls.Add(this.groupBoxListaClientes);
-            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Location = new System.Drawing.Point(10, 11);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1571, 652);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(1178, 530);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Formulário Individual de Restaurante";
@@ -68,21 +70,35 @@
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.label3);
-            this.groupBox4.Location = new System.Drawing.Point(821, 48);
+            this.groupBox4.Location = new System.Drawing.Point(616, 39);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(711, 516);
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox4.Size = new System.Drawing.Size(533, 419);
             this.groupBox4.TabIndex = 8;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Categorias de Menu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 63);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(371, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "aceder a outras opções de gestão do restaurante (como o menu ou pedidos);";
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.listBox1);
             this.groupBox2.Controls.Add(this.groupBox3);
             this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Location = new System.Drawing.Point(418, 40);
+            this.groupBox2.Location = new System.Drawing.Point(314, 32);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(385, 524);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(289, 426);
             this.groupBox2.TabIndex = 7;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Menu de Cada Restaurante";
@@ -90,10 +106,10 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(29, 85);
+            this.listBox1.Location = new System.Drawing.Point(22, 69);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(311, 260);
+            this.listBox1.Size = new System.Drawing.Size(234, 212);
             this.listBox1.TabIndex = 6;
             // 
             // groupBox3
@@ -101,18 +117,21 @@
             this.groupBox3.Controls.Add(this.button1);
             this.groupBox3.Controls.Add(this.label2);
             this.groupBox3.Controls.Add(this.textBox1);
-            this.groupBox3.Location = new System.Drawing.Point(18, 368);
+            this.groupBox3.Location = new System.Drawing.Point(14, 299);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(322, 124);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Size = new System.Drawing.Size(242, 101);
             this.groupBox3.TabIndex = 5;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Criar Novo Menu";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(211, 75);
+            this.button1.Location = new System.Drawing.Point(158, 61);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 26);
+            this.button1.Size = new System.Drawing.Size(62, 21);
             this.button1.TabIndex = 5;
             this.button1.Text = "Criar";
             this.button1.UseVisualStyleBackColor = true;
@@ -120,25 +139,28 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 34);
+            this.label2.Location = new System.Drawing.Point(4, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.Size = new System.Drawing.Size(34, 13);
             this.label2.TabIndex = 0;
             this.label2.Text = "Menu";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(68, 34);
+            this.textBox1.Location = new System.Drawing.Point(51, 28);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(196, 22);
+            this.textBox1.Size = new System.Drawing.Size(148, 19);
             this.textBox1.TabIndex = 5;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(109, 39);
+            this.button2.Location = new System.Drawing.Point(82, 32);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(147, 29);
+            this.button2.Size = new System.Drawing.Size(110, 24);
             this.button2.TabIndex = 3;
             this.button2.Text = "Remover";
             this.button2.UseVisualStyleBackColor = true;
@@ -148,9 +170,11 @@
             this.groupBoxListaClientes.Controls.Add(this.listBoxTrabalhadores);
             this.groupBoxListaClientes.Controls.Add(this.groupBoxNovoCliente);
             this.groupBoxListaClientes.Controls.Add(this.buttonRemoverRestaurante);
-            this.groupBoxListaClientes.Location = new System.Drawing.Point(6, 40);
+            this.groupBoxListaClientes.Location = new System.Drawing.Point(4, 32);
+            this.groupBoxListaClientes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBoxListaClientes.Name = "groupBoxListaClientes";
-            this.groupBoxListaClientes.Size = new System.Drawing.Size(385, 524);
+            this.groupBoxListaClientes.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBoxListaClientes.Size = new System.Drawing.Size(289, 426);
             this.groupBoxListaClientes.TabIndex = 6;
             this.groupBoxListaClientes.TabStop = false;
             this.groupBoxListaClientes.Text = "Gerir Trabalhadores";
@@ -158,10 +182,10 @@
             // listBoxTrabalhadores
             // 
             this.listBoxTrabalhadores.FormattingEnabled = true;
-            this.listBoxTrabalhadores.ItemHeight = 16;
-            this.listBoxTrabalhadores.Location = new System.Drawing.Point(29, 85);
+            this.listBoxTrabalhadores.Location = new System.Drawing.Point(22, 69);
+            this.listBoxTrabalhadores.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBoxTrabalhadores.Name = "listBoxTrabalhadores";
-            this.listBoxTrabalhadores.Size = new System.Drawing.Size(311, 260);
+            this.listBoxTrabalhadores.Size = new System.Drawing.Size(234, 212);
             this.listBoxTrabalhadores.TabIndex = 6;
             // 
             // groupBoxNovoCliente
@@ -169,18 +193,21 @@
             this.groupBoxNovoCliente.Controls.Add(this.buttonCriarRestaurantes);
             this.groupBoxNovoCliente.Controls.Add(this.label1);
             this.groupBoxNovoCliente.Controls.Add(this.textBoxNomeClienteNovo);
-            this.groupBoxNovoCliente.Location = new System.Drawing.Point(18, 368);
+            this.groupBoxNovoCliente.Location = new System.Drawing.Point(14, 299);
+            this.groupBoxNovoCliente.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBoxNovoCliente.Name = "groupBoxNovoCliente";
-            this.groupBoxNovoCliente.Size = new System.Drawing.Size(322, 124);
+            this.groupBoxNovoCliente.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBoxNovoCliente.Size = new System.Drawing.Size(242, 101);
             this.groupBoxNovoCliente.TabIndex = 5;
             this.groupBoxNovoCliente.TabStop = false;
             this.groupBoxNovoCliente.Text = "Criar Novo Trabalhador";
             // 
             // buttonCriarRestaurantes
             // 
-            this.buttonCriarRestaurantes.Location = new System.Drawing.Point(211, 75);
+            this.buttonCriarRestaurantes.Location = new System.Drawing.Point(158, 61);
+            this.buttonCriarRestaurantes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonCriarRestaurantes.Name = "buttonCriarRestaurantes";
-            this.buttonCriarRestaurantes.Size = new System.Drawing.Size(82, 26);
+            this.buttonCriarRestaurantes.Size = new System.Drawing.Size(62, 21);
             this.buttonCriarRestaurantes.TabIndex = 5;
             this.buttonCriarRestaurantes.Text = "Criar";
             this.buttonCriarRestaurantes.UseVisualStyleBackColor = true;
@@ -188,44 +215,39 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 34);
+            this.label1.Location = new System.Drawing.Point(4, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome";
             // 
             // textBoxNomeClienteNovo
             // 
-            this.textBoxNomeClienteNovo.Location = new System.Drawing.Point(68, 34);
+            this.textBoxNomeClienteNovo.Location = new System.Drawing.Point(51, 28);
+            this.textBoxNomeClienteNovo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxNomeClienteNovo.Multiline = true;
             this.textBoxNomeClienteNovo.Name = "textBoxNomeClienteNovo";
-            this.textBoxNomeClienteNovo.Size = new System.Drawing.Size(196, 22);
+            this.textBoxNomeClienteNovo.Size = new System.Drawing.Size(148, 19);
             this.textBoxNomeClienteNovo.TabIndex = 5;
             // 
             // buttonRemoverRestaurante
             // 
-            this.buttonRemoverRestaurante.Location = new System.Drawing.Point(109, 39);
+            this.buttonRemoverRestaurante.Location = new System.Drawing.Point(82, 32);
+            this.buttonRemoverRestaurante.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonRemoverRestaurante.Name = "buttonRemoverRestaurante";
-            this.buttonRemoverRestaurante.Size = new System.Drawing.Size(147, 29);
+            this.buttonRemoverRestaurante.Size = new System.Drawing.Size(110, 24);
             this.buttonRemoverRestaurante.TabIndex = 3;
             this.buttonRemoverRestaurante.Text = "Remover";
             this.buttonRemoverRestaurante.UseVisualStyleBackColor = true;
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(67, 77);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(469, 16);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "aceder a outras opções de gestão do restaurante (como o menu ou pedidos);";
-            // 
             // Form_IndividualRestaurante
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1596, 677);
+            this.ClientSize = new System.Drawing.Size(1194, 550);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form_IndividualRestaurante";
             this.Text = "Individual de Restaurante";
             this.groupBox1.ResumeLayout(false);
