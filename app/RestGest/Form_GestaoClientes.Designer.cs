71c1c4765722fdf145908117882223d44e36c54f
Diogo Gonçalves <2211879@my.ipleiria.pt>
2022-06-08 08:39:38 +0100

    Campos extra
    Adicionar cidade, pais, código-postal e rua

--- app/RestGest/Form_GestaoClientes.Designer.cs
+++ app/RestGest/Form_GestaoClientes.Designer.cs
@@ -30,25 +30,31 @@
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.listBoxClientes = new System.Windows.Forms.ListBox();
-            this.buttonRemoverCliente = new System.Windows.Forms.Button();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBoxNome = new System.Windows.Forms.TextBox();
-            this.textBoxMorada = new System.Windows.Forms.TextBox();
-            this.textBoxTelemovel = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBoxContribuinte = new System.Windows.Forms.TextBox();
-            this.buttonCriarCliente = new System.Windows.Forms.Button();
-            this.buttonAlterarDados = new System.Windows.Forms.Button();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.listBoxCadeiaRestCliente = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonAlterarDados = new System.Windows.Forms.Button();
+            this.buttonCriarCliente = new System.Windows.Forms.Button();
+            this.textBoxContribuinte = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxTelemovel = new System.Windows.Forms.TextBox();
+            this.textBoxMorada = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonRemoverCliente = new System.Windows.Forms.Button();
+            this.listBoxClientes = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -56,7 +62,7 @@
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(816, 398);
+            this.groupBox1.Size = new System.Drawing.Size(816, 462);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Gestão de Clientes";
@@ -69,31 +75,37 @@
             this.groupBox2.Controls.Add(this.listBoxClientes);
             this.groupBox2.Location = new System.Drawing.Point(6, 19);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(804, 374);
+            this.groupBox2.Size = new System.Drawing.Size(804, 437);
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Lista dos Clientes";
             // 
-            // listBoxClientes
+            // groupBox4
             // 
-            this.listBoxClientes.FormattingEnabled = true;
-            this.listBoxClientes.Location = new System.Drawing.Point(6, 38);
-            this.listBoxClientes.Name = "listBoxClientes";
-            this.listBoxClientes.Size = new System.Drawing.Size(792, 147);
-            this.listBoxClientes.TabIndex = 0;
+            this.groupBox4.Controls.Add(this.listBoxCadeiaRestCliente);
+            this.groupBox4.Location = new System.Drawing.Point(518, 185);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(280, 246);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Cadeias de Restaurantes:";
             // 
-            // buttonRemoverCliente
+            // listBoxCadeiaRestCliente
             // 
-            this.buttonRemoverCliente.Location = new System.Drawing.Point(56, 14);
-            this.buttonRemoverCliente.Name = "buttonRemoverCliente";
-            this.buttonRemoverCliente.Size = new System.Drawing.Size(691, 23);
-            this.buttonRemoverCliente.TabIndex = 1;
-            this.buttonRemoverCliente.Text = "Remover Clientes";
-            this.buttonRemoverCliente.UseVisualStyleBackColor = true;
-            this.buttonRemoverCliente.Click += new System.EventHandler(this.buttonRemoverCliente_Click);
+            this.listBoxCadeiaRestCliente.FormattingEnabled = true;
+            this.listBoxCadeiaRestCliente.Location = new System.Drawing.Point(7, 16);
+            this.listBoxCadeiaRestCliente.Name = "listBoxCadeiaRestCliente";
+            this.listBoxCadeiaRestCliente.Size = new System.Drawing.Size(267, 225);
+            this.listBoxCadeiaRestCliente.TabIndex = 0;
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.label5);
             this.groupBox3.Controls.Add(this.buttonAlterarDados);
             this.groupBox3.Controls.Add(this.buttonCriarCliente);
             this.groupBox3.Controls.Add(this.textBoxContribuinte);
@@ -106,28 +118,67 @@
             this.groupBox3.Controls.Add(this.label1);
             this.groupBox3.Location = new System.Drawing.Point(7, 185);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(504, 183);
+            this.groupBox3.Size = new System.Drawing.Size(504, 247);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Criar Cliente";
             // 
-            // label1
+            // buttonAlterarDados
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nome:";
+            this.buttonAlterarDados.Location = new System.Drawing.Point(6, 217);
+            this.buttonAlterarDados.Name = "buttonAlterarDados";
+            this.buttonAlterarDados.Size = new System.Drawing.Size(491, 23);
+            this.buttonAlterarDados.TabIndex = 9;
+            this.buttonAlterarDados.Text = "ALTERAR DADOS ";
+            this.buttonAlterarDados.UseVisualStyleBackColor = true;
+            this.buttonAlterarDados.Click += new System.EventHandler(this.buttonAlterarDados_Click);
             // 
-            // label2
+            // buttonCriarCliente
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 48);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Morada:";
+            this.buttonCriarCliente.Location = new System.Drawing.Point(6, 192);
+            this.buttonCriarCliente.Name = "buttonCriarCliente";
+            this.buttonCriarCliente.Size = new System.Drawing.Size(491, 22);
+            this.buttonCriarCliente.TabIndex = 8;
+            this.buttonCriarCliente.Text = "CRIAR";
+            this.buttonCriarCliente.UseVisualStyleBackColor = true;
+            this.buttonCriarCliente.Click += new System.EventHandler(this.buttonCriarCliente_Click);
+            // 
+            // textBoxContribuinte
+            // 
+            this.textBoxContribuinte.Location = new System.Drawing.Point(88, 98);
+            this.textBoxContribuinte.Name = "textBoxContribuinte";
+            this.textBoxContribuinte.Size = new System.Drawing.Size(409, 20);
+            this.textBoxContribuinte.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "NºContribuinte:";
+            // 
+            // textBoxTelemovel
+            // 
+            this.textBoxTelemovel.Location = new System.Drawing.Point(71, 72);
+            this.textBoxTelemovel.Name = "textBoxTelemovel";
+            this.textBoxTelemovel.Size = new System.Drawing.Size(426, 20);
+            this.textBoxTelemovel.TabIndex = 5;
+            // 
+            // textBoxMorada
+            // 
+            this.textBoxMorada.Location = new System.Drawing.Point(60, 45);
+            this.textBoxMorada.Name = "textBoxMorada";
+            this.textBoxMorada.Size = new System.Drawing.Size(437, 20);
+            this.textBoxMorada.TabIndex = 4;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(52, 20);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(445, 20);
+            this.textBoxNome.TabIndex = 3;
             // 
             // label3
             // 
@@ -138,95 +189,104 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Telemóvel:";
             // 
-            // textBoxNome
+            // label2
             // 
-            this.textBoxNome.Location = new System.Drawing.Point(52, 20);
-            this.textBoxNome.Name = "textBoxNome";
-            this.textBoxNome.Size = new System.Drawing.Size(445, 20);
-            this.textBoxNome.TabIndex = 3;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cidade:";
             // 
-            // textBoxMorada
+            // label1
             // 
-            this.textBoxMorada.Location = new System.Drawing.Point(60, 45);
-            this.textBoxMorada.Name = "textBoxMorada";
-            this.textBoxMorada.Size = new System.Drawing.Size(437, 20);
-            this.textBoxMorada.TabIndex = 4;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
             // 
-            // textBoxTelemovel
+            // buttonRemoverCliente
             // 
-            this.textBoxTelemovel.Location = new System.Drawing.Point(71, 72);
-            this.textBoxTelemovel.Name = "textBoxTelemovel";
-            this.textBoxTelemovel.Size = new System.Drawing.Size(426, 20);
-            this.textBoxTelemovel.TabIndex = 5;
+            this.buttonRemoverCliente.Location = new System.Drawing.Point(56, 14);
+            this.buttonRemoverCliente.Name = "buttonRemoverCliente";
+            this.buttonRemoverCliente.Size = new System.Drawing.Size(691, 23);
+            this.buttonRemoverCliente.TabIndex = 1;
+            this.buttonRemoverCliente.Text = "Remover Clientes";
+            this.buttonRemoverCliente.UseVisualStyleBackColor = true;
+            this.buttonRemoverCliente.Click += new System.EventHandler(this.buttonRemoverCliente_Click);
             // 
-            // label4
+            // listBoxClientes
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(7, 103);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "NºContribuinte:";
+            this.listBoxClientes.FormattingEnabled = true;
+            this.listBoxClientes.Location = new System.Drawing.Point(6, 38);
+            this.listBoxClientes.Name = "listBoxClientes";
+            this.listBoxClientes.Size = new System.Drawing.Size(792, 147);
+            this.listBoxClientes.TabIndex = 0;
             // 
-            // textBoxContribuinte
+            // textBox1
             // 
-            this.textBoxContribuinte.Location = new System.Drawing.Point(88, 100);
-            this.textBoxContribuinte.Name = "textBoxContribuinte";
-            this.textBoxContribuinte.Size = new System.Drawing.Size(409, 20);
-            this.textBoxContribuinte.TabIndex = 7;
+            this.textBox1.Location = new System.Drawing.Point(44, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(454, 20);
+            this.textBox1.TabIndex = 11;
             // 
-            // buttonCriarCliente
+            // label5
             // 
-            this.buttonCriarCliente.Location = new System.Drawing.Point(6, 126);
-            this.buttonCriarCliente.Name = "buttonCriarCliente";
-            this.buttonCriarCliente.Size = new System.Drawing.Size(491, 22);
-            this.buttonCriarCliente.TabIndex = 8;
-            this.buttonCriarCliente.Text = "CRIAR";
-            this.buttonCriarCliente.UseVisualStyleBackColor = true;
-            this.buttonCriarCliente.Click += new System.EventHandler(this.buttonCriarCliente_Click);
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Pais:";
             // 
-            // buttonAlterarDados
+            // textBox2
             // 
-            this.buttonAlterarDados.Location = new System.Drawing.Point(6, 155);
-            this.buttonAlterarDados.Name = "buttonAlterarDados";
-            this.buttonAlterarDados.Size = new System.Drawing.Size(491, 23);
-            this.buttonAlterarDados.TabIndex = 9;
-            this.buttonAlterarDados.Text = "ALTERAR DADOS ";
-            this.buttonAlterarDados.UseVisualStyleBackColor = true;
-            this.buttonAlterarDados.Click += new System.EventHandler(this.buttonAlterarDados_Click);
+            this.textBox2.Location = new System.Drawing.Point(88, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(409, 20);
+            this.textBox2.TabIndex = 13;
             // 
-            // groupBox4
+            // label6
             // 
-            this.groupBox4.Controls.Add(this.listBoxCadeiaRestCliente);
-            this.groupBox4.Location = new System.Drawing.Point(518, 185);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(280, 183);
-            this.groupBox4.TabIndex = 3;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Cadeias de Restaurantes:";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Codigo-Postal:";
             // 
-            // listBoxCadeiaRestCliente
+            // textBox3
             // 
-            this.listBoxCadeiaRestCliente.FormattingEnabled = true;
-            this.listBoxCadeiaRestCliente.Location = new System.Drawing.Point(7, 16);
-            this.listBoxCadeiaRestCliente.Name = "listBoxCadeiaRestCliente";
-            this.listBoxCadeiaRestCliente.Size = new System.Drawing.Size(267, 160);
-            this.listBoxCadeiaRestCliente.TabIndex = 0;
+            this.textBox3.Location = new System.Drawing.Point(43, 170);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(454, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 173);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Rua:";
             // 
             // Form_GestaoClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(840, 415);
+            this.ClientSize = new System.Drawing.Size(840, 475);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form_GestaoClientes";
             this.Text = "Gestão Clientes";
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
-            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -250,5 +310,11 @@
         private System.Windows.Forms.Button buttonAlterarDados;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.ListBox listBoxCadeiaRestCliente;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
     }
 }
